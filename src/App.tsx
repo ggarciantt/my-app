48bb5c28d372a0bfac97f7fb8d7cda5cc1197d5d
Guilherme Soares Garcia <guilherme.garcia@bs.nttdata.com>
2024-05-24 10:33:48 -0300

    feat: implementing HashRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { MyApp } from "./MyApp";
+import { HashRouter } from "react-router-dom";
+
 function App() {
   return (
-    <>
+    <HashRouter>
       <MyApp />
-    </>
+    </HashRouter>
   );
 }
 
